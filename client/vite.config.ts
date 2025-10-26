f779336104f70d0fd7ddcdbbd00f8b49231206e5
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2025-10-26 20:01:15 -0300

    fix: missing character

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite
+import { defineConfig } from 'vite'
 import path from 'path'
 
 // https://vite.dev/config/
